59c88f54a297ea5d555fd5fb827ea81abf193c9c
ITbob <blanquet.bertrand@gmail.com>
2018-10-08 18:49:36 +0100

    add key api

--- TUI.TimeZone/Source/Api/Google/GoogleTimeZoneApi.cs
+++ TUI.TimeZone/Source/Api/Google/GoogleTimeZoneApi.cs
@@ -33,6 +33,7 @@ namespace TUI.TimeZone.Source.Api.Google
                 + "," + loc.Longitude.ToString().Replace(",", "."));
             this._request.AddParameter("timestamp", ToTimestamp(utcDate));
             this._request.AddParameter("sensor", "false");
+            this._request.AddParameter("key", "AIzaSyDsxDimKTCdU12yRTz0swC-8eIuUuHHkqg");
             var response = this._client.Execute<GoogleTimeZone>(this._request);
             return response.Data;
         }
